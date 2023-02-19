0a9b9d4cd69d846d4f8838c34fbd393189a0aa3c
Rohan Rah Mudvari <mudvari25@gmail.com>
2023-02-19 17:57:58 +0545

    Splash

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:splashscreen/main.dart';
 
-class Splash extends StatelessWidget{
+class Splash extends StatefulWidget{
   const Splash ({Key? key}): super(key: key);
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState(){
+    super.initState();
+    _navigatetohome();
+  }
+
+  _navigatetohome()async{
+  await Future.delayed(Duration(milliseconds: 1500), (){});
+  Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>const MyHomePage(title:'GFG')));
+  }
   @override
   Widget build(BuildContext context){
     return Scaffold(
